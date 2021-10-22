c687fc20e09fc60e3f8b0651779c4fe92707fe95
Carlos Ramirez <carlosdanielrt@gmail.com>
2021-10-22 15:37:17 -0400

    [ADD] project: scaffold

---
+++ intec-proyecto-final-t-3/intec-proyecto-final-t-3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace intec_proyecto_final_t_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
