f5d0d47fb484cfa2ccbcea485bbecfe49b5199c4
Carlos Ramirez <carlosdanielrt@gmail.com>
2021-10-24 12:00:32 -0400

    [ADD] project: states model

--- intec-proyecto-final-t-3/intec-proyecto-final-t-3/Models/GeneralDbContext.cs
+++ intec-proyecto-final-t-3/intec-proyecto-final-t-3/Models/GeneralDbContext.cs
@@ -3,10 +3,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace intec_proyecto_final_t_3.Models
 {
-    public class CountriesDbContext : DbContext
+    public class GeneralDbContext : DbContext
     {
         public DbSet<Countries> Countries { get; set; }
-        public CountriesDbContext(DbContextOptions<CountriesDbContext> options) : base(options)
+        public DbSet<States> States { get; set; }
+        public GeneralDbContext(DbContextOptions<GeneralDbContext> options) : base(options)
         {
         }
     }
---
+++ intec-proyecto-final-t-3/intec-proyecto-final-t-3/Models/States.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace intec_proyecto_final_t_3.Models
+{
+    public class States
+    {
+        public Int32 Id { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public String Name { get; set; }
+
+        [Required]
+        [Display(Name = "Country")]
+        [ForeignKey("Countries")]
+        public Int32 CountryId { get; set; }
+    }
+}
