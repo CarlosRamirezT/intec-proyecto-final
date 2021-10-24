ed55b5c5e70dc2781c2909eda335ef68ad059e78
Carlos Ramirez <carlosdanielrt@gmail.com>
2021-10-24 17:07:54 -0400

    [FIX] project: wrong field name

--- intec-proyecto-final-t-3/intec-proyecto-final-t-3/Models/Countries.cs
+++ intec-proyecto-final-t-3/intec-proyecto-final-t-3/Models/Countries.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace intec_proyecto_final_t_3.Models
 {
