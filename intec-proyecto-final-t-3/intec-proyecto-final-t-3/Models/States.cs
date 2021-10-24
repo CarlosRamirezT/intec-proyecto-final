f988ce007c25fa95b6ad622daae1b96ccb3e9e2f
Carlos Ramirez <carlosdanielrt@gmail.com>
2021-10-24 12:31:15 -0400

    [ADD] project: states mvc

--- intec-proyecto-final-t-3/intec-proyecto-final-t-3/Models/States.cs
+++ intec-proyecto-final-t-3/intec-proyecto-final-t-3/Models/States.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace intec_proyecto_final_t_3.Models
 {
